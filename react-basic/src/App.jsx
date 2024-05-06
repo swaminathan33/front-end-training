e92763e777bf92b94d6d675bebb90da8a52adf5c
swaminathan33 <swamiandems@gmail.com>
2024-05-06 21:40:27 +0530

    cv app

--- react-basic/src/App.jsx
+++ react-basic/src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import Todo from './Components/Todo'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [state, setState] = useState(0);
+  const increment = () =>{
+    setState(state + 1)
+  }
+  const decrement = () => {
+    setState(state - 1)
+  }
   return (
     <div>
-      <Todo />
+      <button onClick={increment}>+</button>
+      {state}
+      <button onClick={decrement}>-</button>
     </div>
   )
 }
